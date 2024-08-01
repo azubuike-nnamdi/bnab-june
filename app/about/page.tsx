6e55c458ab8e4c8e0c6541ab8d31f26519a19cdf
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-01 18:04:37 +0100

    chore: config pages :racehorse:

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolores, animi a sunt consectetur sit, fuga tempore officiis alias aspernatur ipsum. Reiciendis consequuntur ex corrupti, deleniti facere sequi sapiente quas!
+      Adipisicing et qui culpa labore anim do elit consequat voluptate nisi proident eiusmod labore Lorem. Non laborum sint elit non in tempor et irure quis excepteur ullamco labore. Sunt sit ad anim eu magna veniam nulla esse.
+      Do aute mollit ut magna non dolore nisi quis. Qui occaecat consectetur esse commodo ullamco excepteur exercitation qui. Non non voluptate est proident proident veniam commodo ut cupidatat eu non magna ut. Exercitation nisi do officia eu nulla sunt ea ad. Ad commodo proident cillum reprehenderit est deserunt aliqua ex ipsum in culpa esse. Nulla sunt veniam adipisicing amet exercitation excepteur do non consectetur proident cupidatat culpa esse voluptate.
+    </div>
+  )
+}
+
+export default page
