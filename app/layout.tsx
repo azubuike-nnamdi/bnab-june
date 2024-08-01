8586fd0e0d79a38a2d3ff03a6cb1bcab57904f04
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-01 18:03:19 +0100

    chore: config project layout across board :racehorse:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/common/navbar";
+import TanstackProvider from "@/context/tanstackProvider";
+import Footer from "@/components/common/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+        <TanstackProvider>
+          <Navbar />
+          <main className="py-24">
+            {children}
+          </main>
+          <Footer />
+        </TanstackProvider>
+      </body>
     </html>
   );
 }
