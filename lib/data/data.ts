e29d2a3b2554beb37818b9951cdb618e84a35346
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-01 18:08:48 +0100

    chore: dummy data :racehorse:

---
+++ lib/data/data.ts
@@ -0,0 +1,277 @@
+
+export const menuItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    path: "/services",
+  },
+  {
+    id: 3,
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    id: 4,
+    title: "Booking",
+    path: "#",
+    subMenu: [
+      {
+        id: 41,
+        title: "Ticket Master",
+        path: "/ticket-master",
+      },
+      { id: 42, title: "Airport Pickup & Dropoff", path: "/airport-pickup-dropoff" },
+      {
+        id: 43,
+        title: "Accommodation",
+        path: "accommodation",
+      },
+      { id: 44, title: "Dedicated Rides", path: "dedicated-rides" },
+      { id: 45, title: "Training", path: "/training" },
+    ],
+  },
+  {
+    id: 5,
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+export const socialMediaPlatforms = [
+  {
+    id: 1,
+    name: "Facebook",
+    img: "/img/facebook.png",
+    href: "#",
+  },
+  { id: 2, name: "Twitter", img: '/img/twitter.png', href: "#" },
+  {
+    id: 3,
+    name: "Instagram",
+    img: "/img/facebook.png",
+    href: "#",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    img: "/img/linkedin.png",
+    href: "#",
+  },
+];
+
+export const links1 = [
+  { id: 1, text: "About us", href: "/about" },
+  // { id: 2, text: "Our offerings", href: "#fleet-list" },
+  // { id: 3, text: "Newsroom", href: "#blog-grid" },
+  { id: 4, text: "Investors", href: "#our-team" },
+  { id: 5, text: "Careers", href: "#" },
+  { id: 6, text: "Blog", href: "#blog-grid" },
+  // { id: 7, text: "Gift cards", href: "#" },
+];
+export const links2 = [
+  { id: 1, name: "Accra", href: "#" },
+  { id: 2, name: "Kumasi", href: "#" },
+  { id: 3, name: "Tamale", href: "#" },
+  { id: 4, name: "Tema", href: "#" },
+  // { id: 5, name: "Achimota", href: "#" },
+];
+export const links3 = [
+  { id: 1, name: "Airport Pickup & Dropoff", href: "#" },
+  { id: 2, name: "Accommodation", href: "#" },
+  { id: 3, name: "Dedicated Rides", href: "#" },
+  { id: 4, name: "Ticket Master", href: "#" },
+  { id: 5, name: "Training", href: "#" },
+  // { id: 6, name: "Airport transfer", href: "#" },
+];
+export const links4 = [
+  { id: 1, name: "Saloon", href: "#" },
+  { id: 2, name: "SUV", href: "#" },
+  { id: 3, name: "Coach", href: "#" },
+  { id: 4, name: "Mini Bus", href: "#" },
+];
+export const legalLinks = [
+  { id: 1, name: "Terms", href: "#about" },
+  { id: 2, name: "Privacy policy", href: "#about" },
+  { id: 3, name: "Legal notice", href: "#about" },
+  { id: 4, name: "Accessibility", href: "#about" },
+];
+
+
+export const ticketmaster = [
+  {
+    id: 1,
+    url: "https://bnab-june.s3.amazonaws.com/img/transfer-2.png",
+    // title: "Embark on an Unforgettable Journey",
+    text: "Live the Moment",
+  },
+  {
+    id: 2,
+    url: "https://bnab-june.s3.amazonaws.com/img/event-2.png",
+    // title: "Adventure Awaits",
+    text: "Unforgettable Moments",
+  },
+  {
+    id: 3,
+    url: "https://bnab-june.s3.amazonaws.com/img/event-6.png",
+    // title: "Luxury Redefined",
+    text: "Experience the Magic Live",
+  },
+  {
+    id: 4,
+    url: "https://bnab-june.s3.amazonaws.com/img/event-4.png",
+    // title: "Serenity at Its Best",
+    text: "Create Memories Together",
+  },
+  {
+    id: 5,
+    url: "https://bnab-june.s3.amazonaws.com/img/event-5.png",
+    // title: "Taste the Adventure",
+    text: "Unleash the Excitement",
+  },
+];
+
+export const airport = [
+  {
+    id: 1,
+    url: "https://bnab-june.s3.amazonaws.com/img/airport1.png",
+    // title: "BANNER 2",
+    text: "A Stress-free Journey",
+  },
+  {
+    id: 2,
+    url: "https://bnab-june.s3.amazonaws.com/img/airportpick-2.png",
+    // title: "BANNER 2",
+    text: "We Get You There",
+  },
+  {
+    id: 3,
+    url: "https://bnab-june.s3.amazonaws.com/img/airportpick-3.png",
+    // title: "BANNER 2",
+    text: "Convenience at Your Fingertips",
+  },
+  {
+    id: 4,
+    url: "https://bnab-june.s3.amazonaws.com/img/airportpick-4.png",
+    // title: "BANNER 2",
+    text: "Airport Pickup & Dropoff",
+  },
+  {
+    id: 5,
+    url: "https://bnab-june.s3.amazonaws.com/img/port-6.png",
+    // title: "BANNER 2",
+    text: "Your Ride Awaits",
+  },
+];
+
+export const accommodation = [
+  {
+    id: 1,
+    url: "https://bnab-june.s3.amazonaws.com/img/room-1.png",
+    // title: "BANNER 3",
+    text: "Experience True Hospitality",
+  },
+  {
+    id: 2,
+    url: "https://bnab-june.s3.amazonaws.com/img/accommodate-2.png",
+    // title: "BANNER 3",
+    text: "Discover Comfort",
+  },
+  {
+    id: 3,
+    url: "https://bnab-june.s3.amazonaws.com/img/accommodation3.png",
+    // title: "BANNER 3",
+    text: "Perfect Place to Rest",
+  },
+  {
+    id: 4,
+    url: "https://bnab-june.s3.amazonaws.com/img/accommodation4.png",
+    // title: "BANNER 3",
+    text: "Comfortable Rooms",
+  },
+  {
+    id: 5,
+    url: "https://bnab-june.s3.amazonaws.com/img/room-5.png",
+    // title: "BANNER 3",
+    text: "Peace and Relaxation",
+  },
+];
+
+export const training = [
+  {
+    id: 1,
+    url: "https://bnab-june.s3.amazonaws.com/img/skills-1.png",
+    // title: "BANNER 4",
+    text: "Master New Skills",
+  },
+  {
+    id: 2,
+    url: "https://bnab-june.s3.amazonaws.com/img/skills-6.png",
+    // title: "BANNER 4",
+    text: "Unlock Your Potential",
+  },
+  {
+    id: 3,
+    url: "https://bnab-june.s3.amazonaws.com/img/skills-3.png",
+    // title: "BANNER 4",
+    text: "Specialized Training",
+  },
+  {
+    id: 4,
+    url: "https://bnab-june.s3.amazonaws.com/img/skills-7.png",
+    // title: "BANNER 4",
+    text: "Become Your Best",
+  },
+  {
+    id: 5,
+    url: "https://bnab-june.s3.amazonaws.com/img/skills-5.png",
+    // title: "BANNER 4",
+    text: "Skill Development",
+  },
+];
+
+export const transport = [
+  {
+    id: 1,
+    url: "https://bnab-june.s3.amazonaws.com/img/portservice-7.png",
+    // title: "BANNER 5",
+    text: "Get There with Ease",
+  },
+  {
+    id: 2,
+    url: "https://bnab-june.s3.amazonaws.com/img/portservice-8.png",
+    // title: "BANNER 5",
+    text: "Wherever You Need to Go",
+  },
+  {
+    id: 3,
+    url: "https://bnab-june.s3.amazonaws.com/img/transfer-3.png",
+    // title: "BANNER 5",
+    text: "Transport Transfer",
+  },
+  {
+    id: 4,
+    url: "https://bnab-june.s3.amazonaws.com/img/transfer-4.png",
+    // title: "BANNER 5",
+    text: "Travel Without Worries",
+  },
+  {
+    id: 5,
+    url: "https://bnab-june.s3.amazonaws.com/img/portservice-9.png",
+    // title: "BANNER 5",
+    text: "Effortless Travel",
+  },
+];
+
+export const tabs = [
+  { name: "Ticket Master", banners: ticketmaster },
+  { name: "Airport Pickup & Dropoff", banners: airport },
+  { name: "Accommodation", banners: accommodation },
+  { name: "⁠Dedicated Rides", banners: transport },
+  { name: "Training", banners: training },
+];
+
