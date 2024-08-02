1311cbf281c4a5f3c5a13c48b51e78a08ac5cd1a
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-02 15:52:40 +0100

    chore: include image protocol :racehorse:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bnab-june.s3.amazonaws.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  }
+};
 
 export default nextConfig;
