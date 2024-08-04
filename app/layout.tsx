de071bc17ccfec46faf9082e048c8b059faffa4f
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-04 07:19:03 +0100

    chore: config project layout :zap:

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import TanstackProvider from "@/context/tanstackProvider";
-import 'swiper/css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const fontSans = FontSans({
   subsets: ["latin"],
