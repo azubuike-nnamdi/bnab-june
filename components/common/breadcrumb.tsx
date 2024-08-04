a77d52edbddba3b6d90b37670c1c54b6a77602e5
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-04 10:03:39 +0100

    feat: breadcrumb :sparkles:

--- components/common/breadcrumb.tsx
+++ components/common/breadcrumb.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+'use client';
 
-export const BreadCrumb = () => {
+import { BreadCrumbProps } from '@/types/declaration';
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '../ui/breadcrumb';
+import { usePathname } from 'next/navigation';
+
+export const BreadCrumb = ({ title, links }: BreadCrumbProps) => {
+  const pathname = usePathname();
   return (
-    <div>BreadCrumb</div>
-  )
-}
+    <main>
+      <section className="w-full bg-black py-4">
+        <div className="container mx-auto sm:p-24 p-8">
+          <h1 className="text-white sm:text-6xl text-2xl mb-4">{title}</h1>
+          <Breadcrumb>
+            <BreadcrumbList className="flex flex-wrap items-center text-white">
+              {links.map((link, index) => (
+                <React.Fragment key={link.id}>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href={link?.path} className='hover:text-gray-400'>{link?.text}</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  {index < links.length - 1 && <BreadcrumbSeparator className="mx-2">/</BreadcrumbSeparator>}
+                </React.Fragment>
+              ))}
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </section>
+    </main>
+  );
+};
---
+++ lib/data/breadcrumb.ts
@@ -0,0 +1,27 @@
+import { ABOUT_URL, DASHBOARD_URL, SERVICES_URL } from "@/config/routes"
+
+export const servicesLink = [
+  {
+    id: 1,
+    path: DASHBOARD_URL,
+    text: 'Home'
+  },
+  {
+    id: 2,
+    path: SERVICES_URL,
+    text: 'Services'
+  }
+]
+
+export const aboutLink = [
+  {
+    id: 1,
+    path: DASHBOARD_URL,
+    text: 'Home'
+  },
+  {
+    id: 2,
+    path: ABOUT_URL,
+    text: 'About Us'
+  }
+]
