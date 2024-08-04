2a7032f83ed7a10bdacd6e17addd57f8dbac51c6
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-04 07:47:07 +0100

    chore: include feature mock data :green_heart:

---
+++ lib/data/feature-data.ts
@@ -0,0 +1,218 @@
+export const features = [
+  {
+    id: 1,
+    icon: "/img/safe.svg",
+    title: "Safety First",
+    description:
+      "Offering efficient and dependable transportation solutions for local and long-distance journeys.",
+  },
+  {
+    id: 2,
+    icon: "/img/price.svg",
+    title: "Prices With No Surprises",
+    description:
+      "Arranging a variety of accommodation options to suit different preferences and budgets.",
+  },
+  {
+    id: 3,
+    icon: "/img/vehicle.svg",
+    title: "Travel Solutions",
+    description:
+      "Providing reliable and stress-free travel experience to and from the airport.",
+  },
+];
+export var features2 = [
+  {
+    id: 1,
+    image: "/img/limo.svg",
+    text: "Luxury Limousine Selection",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+  {
+    id: 2,
+    image: "/img/phone.svg",
+    text: "24/7 Order Available",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+  {
+    id: 3,
+    image: "/img/wheel.svg",
+    text: "Fast Car Delivery Service",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+  {
+    id: 4,
+    image: "/img/seat.svg",
+    text: "High Safety and Purity",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+  {
+    id: 5,
+    image: "/img/price.svg",
+    text: "Fixed Price & Bonus System",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+  {
+    id: 6,
+    image: "/img/driver.svg",
+    text: "Professional Car Drivers",
+    desc: "Both you and your shipments will travel with professional drivers.",
+  },
+];
+export const features3 = ["Affordable", "Punctual", "Professional"];
+export const features4 = [
+  { id: 1, name: "Business Class" },
+  { id: 2, name: "First Class" },
+  { id: 3, name: "Business Van/SUV" },
+  { id: 4, name: "Electric Class" },
+];
+export const features5 = [
+  {
+    id: 1,
+    src: "/img/route.svg",
+    alt: "luxride",
+    title: "Safety First",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+  {
+    id: 2,
+    src: "/img/price.svg",
+    alt: "luxride",
+    title: "Prices With No Surprises",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+  {
+    id: 3,
+    src: "/img/vehicle.svg",
+    alt: "luxride",
+    title: "Private Travel Solutions",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+];
+export const features6 = [
+  {
+    id: 1,
+    iconSrc: "/img/icon1.svg",
+    title: "Safety First",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+  {
+    id: 2,
+    iconSrc: "/img/icon2.svg",
+    title: "Prices With No Surprises",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+  {
+    id: 3,
+    iconSrc: "/img/icon3.svg",
+    title: "Private Travel Solutions",
+    description:
+      "Both you and your shipments will travel with professional drivers. Always with the highest quality standards.",
+  },
+];
+
+export const features7 = [
+  "Smooth Booking Process",
+  "Exclusive Deals",
+  "User-friendly Interface",
+  "Secure Payments",
+  "Customer Support",
+];
+export const features8 = [
+  [{
+    id: 1,
+    imgSrc: "/img/price.svg",
+    title: "All-inclusive pricing",
+    desc: "Discover the easiest way to access the best events in town.",
+  },
+  {
+    id: 2,
+    imgSrc: "/img/airport.svg",
+    title: "Seamless booking process",
+    desc: "Quickly find and purchase tickets with our intuitive booking system.",
+  },
+  {
+    id: 3,
+    imgSrc: "/img/terms.svg",
+    title: "Event Notifications",
+    desc: "Stay updated with notifications about upcoming events.",
+  }],
+  [{
+    id: 1,
+    imgSrc: "/img/price.svg",
+    title: "Workshops",
+    desc: "Interactive sessions with tools and equipment used in the industry.",
+  },
+  {
+    id: 2,
+    imgSrc: "/img/airport.svg",
+    title: "Flexible Scheduling",
+    desc: "Flexible, accessible, personalized and effective learning experience.",
+  },
+  {
+    id: 3,
+    imgSrc: "/img/terms.svg",
+    title: "Skills Enhacement",
+    desc: "Creative thinking and innovative idea generation for growth.",
+  }],
+  [{
+    id: 1,
+    imgSrc: "/img/price.svg",
+    title: "Professional Drivers",
+    desc: "Our drivers are experienced, well-trained, and courteous.",
+  },
+  {
+    id: 2,
+    imgSrc: "/img/airport.svg",
+    title: "24/7 Availability",
+    desc: "Services are reliable and available round the clock.",
+  },
+  {
+    id: 3,
+    imgSrc: "/img/terms.svg",
+    title: "Online Booking System",
+    desc: "Easy to use online booking system. Make reservations effortlessly.",
+  }],
+  [{
+    id: 1,
+    imgSrc: "/img/price.svg",
+    title: "Safety & Security",
+    desc: "Feel secure and safe throughout your stay with our safety measures.",
+  },
+  {
+    id: 2,
+    imgSrc: "/img/airport.svg",
+    title: "Competitive Pricing",
+    desc: "We offer competitive pricing without compromising on quality and value.",
+  },
+  {
+    id: 3,
+    imgSrc: "/img/terms.svg",
+    title: "Unmatched Comfort",
+    desc: "Our accommodations are designed with guest comfort in mind.",
+  }],
+  [{
+    id: 1,
+    imgSrc: "/img/price.svg",
+    title: "Variety of Vehicles",
+    desc: "We offer a range of vehicles to suit your needs.",
+  },
+  {
+    id: 2,
+    imgSrc: "/img/airport.svg",
+    title: "Customization",
+    desc: "We offer tailored travel plans for different occasions.",
+  },
+  {
+    id: 3,
+    imgSrc: "/img/terms.svg",
+    title: "Safety and Cleanliness",
+    desc: "We maintain high standards of safety and hygiene.",
+  }],
+
+];
