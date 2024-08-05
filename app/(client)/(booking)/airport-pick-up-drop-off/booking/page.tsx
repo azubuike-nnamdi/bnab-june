113c75f3c3f03b64d45bbd49ffffb5f50d357ce8
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-05 10:20:08 +0100

    chore: airport booking :green_heart:

--- app/(client)/(booking)/airport-pick-up-drop-off/booking/page.tsx
+++ app/(client)/(booking)/airport-pick-up-drop-off/booking/page.tsx
@@ -1,10 +1,14 @@
+import AirportBooking from "@/components/airport/airportBooking";
 import { BreadCrumb } from "@/components/common/breadcrumb";
+import Faq from "@/components/common/faq";
 import { airportBookingLink } from "@/lib/data/breadcrumb";
 
 export default function page() {
   return (
     <main className="">
       <BreadCrumb title="Airport Pickup & Dropoff Booking" links={airportBookingLink} />
+      <AirportBooking />
+      <Faq />
     </main>
   )
 }
