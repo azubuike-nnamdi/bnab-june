7c132f29f310cd2e568271f440b8494c1637eca7
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-09 09:06:24 +0100

    chore: config get method :racehorse:

--- app/api/v1/ticket-master/route.ts
+++ app/api/v1/ticket-master/route.ts
@@ -1,7 +1,8 @@
 import { getServerSession } from "next-auth";
 import { options } from "../../auth/[...nextauth]/options";
 import clientPromise from "@/lib/db";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { genId } from "@/lib/helper";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(options)
@@ -21,6 +22,7 @@ export async function POST(req: NextRequest) {
       lastName,
       email,
       phoneNumber,
+      event
     } = data;
 
     //validate the fields
@@ -29,6 +31,7 @@ export async function POST(req: NextRequest) {
       "lastName",
       "email",
       "phoneNumber",
+      "event"
     ];
     for (const field of requiredFields) {
       if (!data[field]) {
@@ -37,6 +40,9 @@ export async function POST(req: NextRequest) {
         });
       }
     }
+
+    // Generate unique ID for the booking
+    const bookingId = genId();
 
     //connect to db
     const client = await clientPromise;
@@ -55,11 +61,16 @@ export async function POST(req: NextRequest) {
 
     //create a new ticket object
     const ticket = {
+      id: bookingId,
       firstName,
       lastName,
       email,
       phoneNumber,
+      event,
+      paymentStatus: "not paid",
       createdAt: new Date(),
+      updatedAt: new Date(),
+
     };
 
     //insert the new user into the db
@@ -68,6 +79,50 @@ export async function POST(req: NextRequest) {
     return new Response(JSON.stringify({ message: "Ticket created successfully" }), {
       status: 201,
     });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ message: "Something went wrong" }), {
+      status: 500,
+    });
+  }
+}
+
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(options);
+
+  // Check user session
+  if (!session) {
+    return new Response(JSON.stringify({ message: "Session not active" }), {
+      status: 401,
+    });
+  }
+
+  try {
+    const client = await clientPromise;
+    const db = client.db();
+    const ticketBooking = db.collection("ticket-booking");
+
+    // Retrieve ID from query params if present
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+
+    let tickets;
+
+    if (id) {
+      // Fetch a specific ticket by ID
+      tickets = await ticketBooking.findOne({ id });
+      if (!tickets) {
+        return new Response(JSON.stringify({ message: "Ticket not found" }), {
+          status: 404,
+        });
+      }
+    } else {
+      // Fetch all tickets
+      tickets = await ticketBooking.find().toArray();
+    }
+
+    return new NextResponse(JSON.stringify(tickets), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response(JSON.stringify({ message: "Something went wrong" }), {
