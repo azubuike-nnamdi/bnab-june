2160ca062eb78dd301ffed78911c4c5482601698
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-11 08:50:19 +0100

    chore: include callback :green_heart:

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,8 +3,21 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GithubProvider from 'next-auth/providers/github';
 import TwitterProvider from 'next-auth/providers/twitter';
 import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from 'next-auth';
+import { DefaultSession, NextAuthOptions } from 'next-auth';
 import clientPromise from "@/lib/db";
+
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role?: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    role?: string;
+  }
+}
 
 export const options: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -28,5 +41,24 @@ export const options: NextAuthOptions = {
     strategy: 'database',
     maxAge: 1 * 24 * 60 * 60 // 1 days
   },
+  callbacks: {
+    async signIn({ user }) {
+      const client = await clientPromise;
+      const db = client.db();
 
+      await db.collection("users").updateOne(
+        { email: user.email },
+        {
+          $set: {
+            lastActive: new Date(),
+          },
+        }
+      );
+      if (!user.role) {
+        user.role = "user";
+      }
+
+      return true;
+    }
+  },
 }
