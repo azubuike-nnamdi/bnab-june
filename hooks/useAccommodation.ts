6c236e2344c2a5197f42f53e6f9611495f0c6b35
iamclement1 <iamnnamdiclement@gmail.com>
2024-08-11 08:54:21 +0100

    feat: query to fetch accommodation data :racehorse:

---
+++ hooks/useAccommodation.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const useAccommodation = () => {
+  const fetchAccommodation = async () => {
+    const data = await axios.get("/api/v1/accommodation");
+    return data;
+  };
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["accommodation"],
+    queryFn: fetchAccommodation,
+    staleTime: 300000,
+  });
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useAccommodation;
