e328889b72f43c58da786f3e6bbbe8153d0b4df9
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-08-24 00:00:05 +0100

    chore: include mobile nav :zap:

--- components/common/menu.tsx
+++ components/common/menu.tsx
@@ -3,11 +3,14 @@ import React, { useState } from 'react'
 import { Search } from '../back-office/dashboard/search'
 import { UserNav } from '../back-office/dashboard/user-nav'
 import { ModeToggle } from '../ui/mode-toggle'
+import BackOfficeMobileNav from '../back-office/mobile-nav'
 
 export const Menu = () => {
   const [query, setQuery] = useState("");
   return (
-    <main className='border-b px-2 py-4 md:flex justify-end items-center overflow-x-auto'>
+    <main className='border-b px-2 py-4 flex justify-between items-center '>
+
+      <BackOfficeMobileNav />
       <section className='flex gap-3 items-center px-4'>
         {/* <Search
           searchQuery={query}
