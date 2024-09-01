c0aa902f8582d64fe8864200976f549c21ac9589
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-01 19:27:34 +0100

    chore: include more mutations :green_heart:

--- hooks/mutations/useSubmitTransaction.ts
+++ hooks/mutations/useSubmitTransaction.ts
@@ -3,32 +3,39 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { useAirportBooking } from "@/hooks/mutations/useAirportBooking";
+import { useAccommodationBooking } from "@/hooks/mutations/useAccommodationBooking";
+import { TransactionType, TransactionDataMap } from "@/types/declaration";
+import { useSubmitBooking } from "./useSubmitBooking";
+import { useSubmitTicket } from "./useSubmitTicket";
 
-export const useSubmitTransaction = (transactionType: string) => {
+
+export const useSubmitTransaction = <T extends TransactionType>(transactionType: T) => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
   // Map transaction types to their respective mutation hooks
-  const mutationHooks = {
+  const mutationHooks: Record<TransactionType, () => any> = {
     airportBooking: useAirportBooking,
+    accommodation: useAccommodationBooking,
+    booking: useSubmitBooking,
+    ticket: useSubmitTicket
   };
 
-  const useMutationHook = mutationHooks[transactionType as keyof typeof mutationHooks];
-
+  const useMutationHook = mutationHooks[transactionType];
   const { mutate, isPending, error } = useMutationHook();
 
-
-  const handleSubmitTransaction = (formData: any) => {
+  // Ensure that the formData is of the correct type based on the transactionType
+  const handleSubmitTransaction = (formData: TransactionDataMap[T]) => {
     mutate(formData, {
-      onSuccess: ({ data }) => {
+      onSuccess: ({ data }: { data: any }) => {
         if (data) {
-          toast.success(data?.message);
+          toast.success(data.message);
           router.push("/");
         }
         queryClient.invalidateQueries({ queryKey: [transactionType] });
       },
       onError: (error: any) => {
-        const errorMsg = error?.response?.data?.message || 'An error occurred';
+        const errorMsg = error?.response?.data?.message || "An error occurred";
         toast.error(errorMsg);
       },
     });
