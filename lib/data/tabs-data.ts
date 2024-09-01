b429ae7f4a833472dea8c66db8a41352f7b3863f
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-01 19:28:13 +0100

    chore: include more tab :zap:

--- lib/data/tabs-data.ts
+++ lib/data/tabs-data.ts
@@ -1,12 +1,17 @@
 export const tabs = [
   {
     id: 1,
-    text: "Billing Information",
-    value: "billing",
+    text: "Passenger Information",
+    value: "passenger",
   },
   {
     id: 2,
     text: "Summary",
     value: "summary",
   },
+  {
+    id: 3,
+    text: "Billing Information",
+    value: "billing",
+  }
 ];
