2ebcd7f0e89dbe3457a8dbc654d08a7caa2896e3
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-01 19:26:55 +0100

    refactor: checkout context :hammer:

--- context/checkoutContext.tsx
+++ context/checkoutContext.tsx
@@ -1,16 +1,11 @@
-// contexts/CheckoutContext.tsx
 import React, { createContext, useContext, useState } from 'react';
-import { AirportBookingData } from '@/types/declaration';
+import { CheckoutContextType, CheckoutData } from '@/types/declaration';
 
-type CheckoutContextType = {
-  checkout: AirportBookingData | null;
-  setCheckout: (data: AirportBookingData) => void;
-};
 
 const CheckoutContext = createContext<CheckoutContextType | undefined>(undefined);
 
 export const CheckoutProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [checkout, setCheckout] = useState<AirportBookingData | null>(null);
+  const [checkout, setCheckout] = useState<CheckoutData | null>(null);  // Use CheckoutData | null
 
   return (
     <CheckoutContext.Provider value={React.useMemo(() => ({ checkout, setCheckout }), [checkout])}>
@@ -22,7 +17,7 @@ export const CheckoutProvider: React.FC<{ children: React.ReactNode }> = ({ chil
 export const useCheckoutContext = (): CheckoutContextType => {
   const context = useContext(CheckoutContext);
   if (!context) {
-    throw new Error('useTicketMaster must be used within a CheckoutProvider');
+    throw new Error('useCheckoutContext must be used within a CheckoutProvider');
   }
   return context;
 };
