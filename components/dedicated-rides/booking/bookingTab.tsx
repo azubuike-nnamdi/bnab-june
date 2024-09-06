e9eea706f8fc114f1e431ea3827381f6a1f3f6a9
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-06 08:43:13 +0100

    chore: include more fields and monitor form data :green_heart:

--- components/dedicated-rides/booking/bookingTab.tsx
+++ components/dedicated-rides/booking/bookingTab.tsx
@@ -21,6 +21,9 @@ export default function DedicatedRideBookingTab({ car }: any) {
     phoneNumber: "",
     email: "",
     pickUpLocation: "",
+    vehicleType: 'Regular', // Default vehicle type
+    price: car?.price, // Initial price
+    isBookingForSelf: true,
     pickUpDate: format(new Date(), "yyyy-MM-dd"),
     pickUpTime: format(new Date(), "HH:mm"),
     dropOffLocation: "",
@@ -28,6 +31,8 @@ export default function DedicatedRideBookingTab({ car }: any) {
     dropOffTime: format(new Date(), "HH:mm"),
     numberOfPassengers: "",
     additionalInfo: "",
+    bookingForName: "",
+    bookingForPhone: "",
   });
 
   const handleTabClick = (value: string) => {
@@ -43,65 +48,64 @@ export default function DedicatedRideBookingTab({ car }: any) {
     setFormData(data);
   };
 
+  const handleFormDataChange = (updatedData: Partial<DedicatedRideBookingProps>) => {
+    setFormData(prevData => ({ ...prevData, ...updatedData }));
+  };
+
   return (
     <div className="container mx-auto my-8">
-      <Tabs defaultValue="passenger" value={activeTab} onValueChange={handleTabClick} className="w-full bg-white">
-        <TabsList className="flex justify-center space-x-4 bg-white">
-          {tabs.map((elm) => (
-            <TabsTrigger key={elm.id} value={elm.value} className={clsx(
-              "px-4 py-2 sm:text-2xl text-xl font-medium border-b-2",
-              {
-                "font-bold border-b-4 border-black": activeTab === elm.value,
-                "border-gray-400 text-gray-500": activeTab !== elm.value,
-              }
-            )}>
-              {elm.text}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-        <TabsContent value="passenger" className="mt-4">
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/3 p-4">
+      <div className="flex flex-col md:flex-row">
+        {/* Tabs and Main Content */}
+        <div className="md:w-2/3 p-4">
+          <Tabs defaultValue="passenger" value={activeTab} onValueChange={handleTabClick} className="w-full bg-white">
+            <TabsList className="flex justify-center space-x-4 bg-white">
+              {tabs.map((elm) => (
+                <TabsTrigger key={elm.id} value={elm.value} className={clsx(
+                  "px-4 py-2 sm:text-2xl text-xl font-medium border-b-2",
+                  {
+                    "font-bold border-b-4 border-black": activeTab === elm.value,
+                    "border-gray-400 text-gray-500": activeTab !== elm.value,
+                  }
+                )}>
+                  {elm.text}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+
+            {/* Passenger Details Tab */}
+            <TabsContent value="passenger" className="mt-4">
               <PassengerDetails
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
                 onFormSubmit={handleFormSubmit}
+                onFormDataChange={handleFormDataChange}
               />
-            </div>
-            <div className="md:w-1/3 p-4">
-              <BookingSidebar car={car} />
-            </div>
-          </div>
-        </TabsContent>
-        <TabsContent value="summary" className="mt-4">
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/3 p-4">
+            </TabsContent>
+
+            {/* Booking Summary Tab */}
+            <TabsContent value="summary" className="mt-4">
               <BookingSummary
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
                 formData={formData} />
-            </div>
-            <div className="md:w-1/3 p-4">
-              <BookingSidebar car={car} />
-            </div>
-          </div>
-        </TabsContent>
-        <TabsContent value="billing" className="mt-4">
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-2/3 p-4">
+            </TabsContent>
+
+            {/* Payment Method Tab */}
+            <TabsContent value="billing" className="mt-4">
               <PaymentMethod
                 paymentMethod={paymentMethod}
                 onPaymentSelect={handlePaymentSelect}
                 transactionType={transactionType}
                 formData={formData} />
-            </div>
-            <div className="md:w-1/3 p-4">
-              <BookingSidebar car={car} />
-            </div>
-          </div>
-        </TabsContent>
-      </Tabs>
-    </div>
+            </TabsContent>
+          </Tabs>
+        </div>
 
-  )
+        {/* Booking Sidebar */}
+        <div className="md:w-1/3 p-4">
+          <BookingSidebar formData={formData} />
+        </div>
+      </div>
+    </div>
+  );
 }
