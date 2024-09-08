374a0d94a425ae6e49025509b78eda520a1afd53
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-08 12:05:55 +0100

    chore: accommodation dom data :zap:

---
+++ lib/data/accommodation.ts
@@ -0,0 +1,15 @@
+export const budgetOptions = [
+  { id: 1, name: "Economy", price: "$80.00 - $120.00" },
+  { id: 2, name: "Standard", price: "$120.00 - $200.00" },
+  { id: 3, name: "Premium", price: "$200.00 - $300.00" },
+  { id: 4, name: "Luxury", price: "$300.00 - $500.00" },
+];
+
+export const accommodationOptions = [
+  { id: 1, type: "Hotel" },
+  { id: 2, type: "Hostel" },
+  { id: 3, type: "B&B" },
+  { id: 4, type: "Villa" },
+  { id: 5, type: "Guesthouse" },
+  { id: 6, type: "Apartment" },
+]
