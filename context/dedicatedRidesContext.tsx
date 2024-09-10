9c7a1bb05ece22ec00e55c5beed285816f5f801b
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-10 16:32:34 +0100

    chore: change data :zap:

--- context/dedicatedRidesContext.tsx
+++ context/dedicatedRidesContext.tsx
@@ -1,6 +1,9 @@
 // Dedicated rides BookingContext.tsx
 import React, { createContext, useState, useContext, ReactNode } from 'react';
 import { DedicatedRideBookingProps, TransactionType } from '@/types/declaration';
+import { vehiclePrice } from '@/lib/data/car-data';
+import { format } from "date-fns";
+
 
 interface BookingContextType {
   formData: DedicatedRideBookingProps;
@@ -15,27 +18,30 @@ const BookingContext = createContext<BookingContextType | undefined>(undefined);
 
 export const DedicatedBookingProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [formData, setFormData] = useState<DedicatedRideBookingProps>({
+    transactionId: "",
     firstName: "",
     lastName: "",
     phoneNumber: "",
     email: "",
     pickUpLocation: "",
-    vehicleType: 'Regular',
-    price: "0",
+    vehicleType: 'Regular', // Default vehicle type
+    price: vehiclePrice['Regular'], // Initial price
     isBookingForSelf: true,
-    pickUpDate: new Date().toISOString().split('T')[0],
-    pickUpTime: new Date().toISOString().split('T')[1].substring(0, 5),
+    pickUpDate: format(new Date(), "yyyy-MM-dd"),
+    pickUpTime: format(new Date(), "HH:mm"),
     dropOffLocation: "",
-    dropOffDate: new Date().toISOString().split('T')[0],
-    dropOffTime: new Date().toISOString().split('T')[1].substring(0, 5),
-    numberOfPassengers: "",
+    dropOffDate: format(new Date(), "yyyy-MM-dd"),
+    dropOffTime: format(new Date(), "HH:mm"),
+    numberOfPassengers: "3",
     additionalInfo: "",
     numberOfDays: "",
     bookingForFirstName: "",
     bookingForLastName: "",
     bookingForEmail: "",
     bookingForPhoneNumber: "",
-    totalAmount: 0.00,
+    totalAmount: 0, // Initialize totalAmount as a number
+    bookingType: "dedicatedRides",
+    budget: 0,
   });
   const [paymentMethod, setPaymentMethod] = useState<string>('');
   const [transactionType, setTransactionType] = useState<TransactionType>('booking');
