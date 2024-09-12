236e2ce3064cf9d9d7c97a50e8e7227a14c8a6ca
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-12 12:48:35 +0100

    refactor: dashboard :hammer:

--- app/(back-office)/dashboard/page.tsx
+++ app/(back-office)/dashboard/page.tsx
@@ -1,10 +1,6 @@
 import { Metadata } from "next";
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
-import AdminOverView from "@/components/back-office/overview";
-import DedicatedRides from "@/components/back-office/dedicatedRides";
-import AirportDataTable from "@/components/back-office/airportDataTable";
-import AccommodationTable from "@/components/back-office/accommodationTable";
-import { TicketmasterDataTable } from "@/components/back-office/ticketMasterDataTable";
+import { UserChart } from "@/components/back-office/dashboard/piechart";
+import { UserTable } from "@/components/back-office/dashboard/users/userTable";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -18,31 +14,8 @@ export default function Page() {
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         </div>
-        <div className="overflow-x-auto">
-          <Tabs defaultValue="ticket-master" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="ticket-master">Ticket Master</TabsTrigger>
-              <TabsTrigger value="airport">Airport Pickup & Drop-off</TabsTrigger>
-              <TabsTrigger value="accommodation">Accommodation</TabsTrigger>
-              <TabsTrigger value="dedicated-rides">Dedicated Rides</TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview" className="space-y-4">
-              <AdminOverView />
-            </TabsContent>
-            <TabsContent value="ticket-master">
-              <TicketmasterDataTable />
-            </TabsContent>
-            <TabsContent value="airport">
-              <AirportDataTable />
-            </TabsContent>
-            <TabsContent value="accommodation">
-              <AccommodationTable />
-            </TabsContent>
-            <TabsContent value="dedicated-rides">
-              <DedicatedRides />
-            </TabsContent>
-          </Tabs>
-        </div>
+        <UserChart />
+        <UserTable />
       </div>
     </div>
   );
