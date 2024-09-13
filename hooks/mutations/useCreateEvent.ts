b9fbd749655c15fb60de53a4c2c655fc08749656
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-13 18:09:09 +0100

    chore: change api route :zap:

--- hooks/mutations/useCreateEvent.ts
+++ hooks/mutations/useCreateEvent.ts
@@ -1,6 +1,6 @@
 // api.js
 import { DASHBOARD_URL } from "@/config/routes";
-import { AirportBookingData, Event } from "@/types/declaration";
+import { Event } from "@/types/declaration";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -12,7 +12,7 @@ export const useCreateEvent = () => {
   const router = useRouter();
   const mutation = useMutation({
     mutationFn: (event: Event) => {
-      return axios.post("/api/v1/all-events", event);
+      return axios.post("/api/v1/create-bookings/all-events", event);
     },
     onSuccess: ({ data }) => {
       if (data) {
