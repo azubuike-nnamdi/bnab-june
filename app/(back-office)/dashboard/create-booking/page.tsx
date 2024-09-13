1d3628f2d61759b077e7e8d7e6be01b43bb8c16e
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-13 14:01:49 +0100

    feat: include booking route for dashboard :racehorse:

---
+++ app/(back-office)/dashboard/create-booking/page.tsx
@@ -0,0 +1,28 @@
+import CreateEventForm from "@/components/back-office/dashboard/bookings/createEventForm";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function Page() {
+  return (
+    <div className="overflow-x-hidden flex-nowrap whitespace-nowrap flex-col md:flex">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Create Bookings</h2>
+        </div>
+        <div className="overflow-x-auto">
+          <Tabs defaultValue="ticket-master" className="space-y-4">
+            <TabsList className="grid w-[400px] grid-cols-2">
+              <TabsTrigger value="ticket-master">Ticket Master</TabsTrigger>
+              <TabsTrigger value="dedicated-rides">Dedicated Rides</TabsTrigger>
+            </TabsList>
+            <TabsContent value="ticket-master" className="space-y-4">
+              <CreateEventForm />
+            </TabsContent>
+            <TabsContent value="dedicated-rides">
+              lorem
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  )
+}
