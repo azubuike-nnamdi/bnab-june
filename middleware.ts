40bdc4b31317817b657faa819419c62b6e60118e
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-26 12:50:40 +0100

    chore: rename middleware :zap:

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { HOME_URL, LOGIN_URL } from './config/routes'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  let token = request.cookies.get('next-auth.session-token')?.value
+
+  console.log('token', token)
+  if (!token) return NextResponse.redirect(new URL(LOGIN_URL, request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/ticket-master/:path*'],
+}
