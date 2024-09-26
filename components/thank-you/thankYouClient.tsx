c5c7f1f465cf9f55aec943c9aefafd8ef4fbfeaa
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-26 08:22:04 +0100

    chore: delete thank you client component :zap:

--- components/thank-you/thankYouClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import ThankYou from "../common/thankYou";
-
-
-export default function ThankYouClient({ sanitizedReference }: Readonly<{ sanitizedReference: string }>) {
-  return <ThankYou reference={sanitizedReference} />;
-}
