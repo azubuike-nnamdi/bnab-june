be606b162ed1ecbf0fd81c41bf985c136d4ca94a
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-09-27 09:48:31 +0100

    chore: remove unused var :zap:

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { HOME_URL, LOGIN_URL } from './config/routes'
+import { LOGIN_URL } from './config/routes'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
