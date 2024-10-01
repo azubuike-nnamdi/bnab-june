f6ce97045ffa08c7ecc2f86419a465e09e47ef80
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-10-01 21:09:35 +0100

    refactor: fetch data :hammer:

--- components/ticket-master/bookingTicketCard.tsx
+++ components/ticket-master/bookingTicketCard.tsx
@@ -1,30 +1,11 @@
-'use client'
 
-import { BookingTicket, EventCardProps } from "@/types/declaration"
+import { BookingTicket } from "@/types/declaration"
 import { Calendar, MapPin, Phone, Timer } from "lucide-react";
-import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
-import { useState } from "react";
 
 export default function BookingTicketCard({ event }: Readonly<BookingTicket>) {
-
-
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  const handleBooking = async (eventId: string) => {
-    router.push(`/ticket-master/booking/${eventId}`);
-
-    // if (!session) {
-    //   await signIn();
-    //   router.push(`/ticket-master/booking/${eventId}`);
-    // } else {
-    //   router.push(`/ticket-master/booking/${eventId}`);
-    // }
-  };
   return (
     <main>
       <section className="sm:mt-24 sm:px-24 mt-4 px-4">
@@ -33,28 +14,28 @@ export default function BookingTicketCard({ event }: Readonly<BookingTicket>) {
           <div className="mt-12 flex flex-wrap">
             <div className="w-full lg:w-2/3 mb-8">
               <h5 className="sm:text-xl text-lg font-medium mb-2">
-                {event.title}
+                {event?.title}
               </h5>
               <p className="text-sm mb-4">
-                {event.desc}
+                {event?.description}
               </p>
               <div className="mt-1">
                 <div className="my-4 flex items-center">
                   <MapPin />
-                  <span className="mx-2">{event.venue}</span>
+                  <span className="mx-2">{event?.address}</span>
                 </div>
                 <div className="flex gap-3">
                   <div className="flex items-center">
                     <Timer />
-                    <span className="mx-2">{event.time}</span>
+                    <span className="mx-2">{event?.time}</span>
                   </div>
                   <div className="flex items-center">
                     <Calendar />
-                    <span className="mx-2">{event.date}</span>
+                    <span className="mx-2">{event?.date}</span>
                   </div>
                   <div className="flex items-center">
                     <Phone />
-                    <Link className="mx-2" href={`tel:${event.phoneNo}`}>{event.phoneNo}</Link>
+                    <Link className="mx-2" href={`tel:${event?.phoneNumber}`}>{event?.phoneNumber}</Link>
                   </div>
                 </div>
               </div>
@@ -63,7 +44,7 @@ export default function BookingTicketCard({ event }: Readonly<BookingTicket>) {
                   width={700}
                   height={326}
                   className="object-contain"
-                  src={event.img}
+                  src={event?.image}
                   alt="event"
                 />
               </div>
@@ -90,27 +71,29 @@ export default function BookingTicketCard({ event }: Readonly<BookingTicket>) {
                   </li>
                 </ul>
                 <div className="mt-8 text-center">
-                  <Button
-                    className="btn btn-primary w-full"
-                    onClick={() => handleBooking(event.id)}
-                  >
-                    Book Now
-                    <svg
-                      className="w-4 h-4 ml-1 inline-block"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
+                  <Link href={`/ticket-master/booking/${event?._id}`}>
+                    <Button
+                      className="btn btn-primary w-full"
+
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-                      ></path>
-                    </svg>
-                  </Button>
+                      Book Now
+                      <svg
+                        className="w-4 h-4 ml-1 inline-block"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                        ></path>
+                      </svg>
+                    </Button>
+                  </Link>
                 </div>
               </div>
               <div className="flex flex-wrap mt-8">
