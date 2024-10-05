a05ad4a9b6e2e877cd1a825d421a968eef06d28f
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-10-05 23:06:58 +0100

    chore: get user name on dashboard navbar :zap:

--- components/back-office/dashboard/user-nav.tsx
+++ components/back-office/dashboard/user-nav.tsx
@@ -1,42 +1,43 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { LOGIN_URL } from "@/config/routes";
+import { getFirstName } from "@/lib/helper";
+import { ChevronDown } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 
 export function UserNav() {
+  const { data: session } = useSession();
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">FirstName</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <main>
+      {session ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex items-center">
+            {getFirstName(session?.user?.name)}
+
+            <ChevronDown className="ml-1" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <Button
+              className="w-full"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </Button>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <div className="gap-4 flex">
+          <Link href={LOGIN_URL}>
+            <Button>
+              Log In
+            </Button>
+          </Link>
+        </div>
+      )}
+    </main>
+
   )
 }
