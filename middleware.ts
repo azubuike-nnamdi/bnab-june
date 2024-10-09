142b22308446ff5e44e08cee887e293acf4af39a
azubuike-nnamdi <iamnnamdiclement@gmail.com>
2024-10-09 15:09:40 +0100

    chore: integrate middleware :zap:

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { LOGIN_URL } from './config/routes'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  let token = request.cookies.get('next-auth.session-token')?.value
+
+  console.log('token', token)
+  if (!token) return NextResponse.redirect(new URL(LOGIN_URL, request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/ticket-master/booking/:path*', '/airport-pick-up-drop-off/booking/:path*', '/dedicated-rides/booking/:path*', '/checkout/:path*'],
+}
+
