873584d830af39ebbd8de1d6628076857b45c944
Nnamdi Azubuike <nnamdi.azubuike@mtn.com>
2024-10-21 10:00:21 +0100

    chore: check env for token value :zap:

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,13 @@ import { LOGIN_URL } from './config/routes'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  let token = request.cookies.get('next-auth.session-token')?.value
+  // Determine the correct cookie name based on the environment
+  const tokenName = process.env.NODE_ENV === 'production'
+    ? '__Host-next-auth.csrf-token'
+    : 'next-auth.session-token'; // Use the correct token name for local development
+
+  // Get the token from the appropriate cookie
+  const token = request.cookies.get(tokenName)?.value;
 
   if (!token) return NextResponse.redirect(new URL(LOGIN_URL, request.url))
 }
