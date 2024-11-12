157bbaa77d78eabbe1003ff71a9d28ad2b5be4ea
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2024-11-12 07:51:10 +0100

    feat: zone creation page :racehorse:

---
+++ app/(back-office)/dashboard/zone-creation/page.tsx
@@ -0,0 +1,6 @@
+import ZoneCreationForm from "@/components/back-office/dashboard/zone/zone-creation-form";
+import { ZoneHomepage } from "@/components/back-office/dashboard/zone/zone-homepage";
+
+export default function Page() {
+  return <ZoneHomepage />
+}
