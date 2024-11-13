2d9f0e9e7dcbc55a09b6dd6c3d1efb0c60fe9cb9
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2024-11-13 10:45:03 +0100

    chore: change text :zap:

--- components/about/aboutFeature.tsx
+++ components/about/aboutFeature.tsx
@@ -7,7 +7,7 @@ export default function AboutFeature() {
     <main className="sm:py-12 py-4 sm:p-0 p-4">
       <h3 className="sm:text-5xl font-semibold text-center text-gray-800 mb-3">Who We Are</h3>
       <p className="mx-auto sm:w-10/12 text-md">
-        Welcome to Hyea Me Ha, your ultimate travel companion for exploring the vibrant and diverse country of Ghana. Our app is designed to make your travel experience seamless, enjoyable, and unforgettable by offering a wide range of services tailored to meet all your travel needs.
+        Welcome to Hyea Me Ha, your ultimate travel companion for exploring the vibrant and diverse country of Ghana. Our website is designed to make your travel experience seamless, enjoyable, and unforgettable by offering a wide range of services tailored to meet all your travel needs.
       </p>
       <ul className="list-disc list-inside text-lg text-gray-700 dark:text-gray-300 mb-10 sm:px-28 px-4 py-4 sm:mt-14">
         {features7.map((elm) => (
