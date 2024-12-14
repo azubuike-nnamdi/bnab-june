071541d1508882d94a9377bc395944466cd6b4a6
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2024-12-14 19:04:00 +0100

    chore: fetch event by id :zap:

--- app/(client)/(booking)/ticket-master/booking/[id]/page.tsx
+++ app/(client)/(booking)/ticket-master/booking/[id]/page.tsx
@@ -2,14 +2,19 @@
 
 import SkeletonBookingTicketCard from "@/components/common/booking-card-skeleton";
 import TicketBookingTab from "@/components/ticket-master/ticketBookingTab";
+import useGetEventById from "@/hooks/useGetEventsById";
 import useTicketMasterById from "@/hooks/useTicketMasterById";
 import { PageProps } from "@/types/declaration";
 export default function Page({ params }: Readonly<PageProps>) {
   const id = params?.id
 
-  const { data, isPending } = useTicketMasterById({ id });
+  // const { data, isPending } = useTicketMasterById({ id });
 
-  const event = data?.data?.ticket
+  // const event = data?.data?.ticket
+  const { data: singleEvent, isPending, error } = useGetEventById(id);
+
+  // const event = data?.data?.ticket
+  const event = singleEvent?.data
   return (
     <main>
       {isPending ? <SkeletonBookingTicketCard /> : (
